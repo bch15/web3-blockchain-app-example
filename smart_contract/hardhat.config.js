0d0755fe7be191a5753c6665075c376f9780da04
Behzad Chaharbaghi <82478699+BehzadChaharbaghi@users.noreply.github.com>
2022-06-01 11:46:49 +0430

    add env

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,12 +1,14 @@
 //https://eth-ropsten.alchemyapi.io/v2/6LV8HAM4GitKaNB-V7iKKV8cMpx9fsF2
 require('@nomiclabs/hardhat-waffle');
 
+const PRIVATEKEY = import.meta.env.PRIVATE_RINKEBY_ACCOUNT_KEY;
+
 module.exports = {
   solidity: '0.8.0',
-  networks:{
-    ropsten:{
+  networks: {
+    ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/6LV8HAM4GitKaNB-V7iKKV8cMpx9fsF2',
-      accounts: ['f6d64351a5420f7bf253a084fae986eae2f46dc0322a7c7ca9be8eb4819f482d']
+      accounts: [`${PRIVATEKEY}`]
     }
   }
 }
